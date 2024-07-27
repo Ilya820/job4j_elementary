87319ed81096462273e0362e615f7edcd653f35d
Ilya820 <i8145614@gmail.com>
2024-07-27 23:11:30 +0300

    GPT. Нейросети спешат на помощь

--- src/main/java/ru/job4j/condition/RectangleArea.java
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -1,12 +1,12 @@
 package ru.job4j.condition;
 
 public class RectangleArea {
-    public static double square(double p, double k) {
-        return (p / (2 * (k + 1))) * k * (p / (2 * (k + 1)));
+    public static double square(double width, double height) {
+        return (width / (2 * (height + 1))) * height * (width / (2 * (height + 1)));
     }
 
     public static void main(String[] args) {
         double result = RectangleArea.square(4, 1);
-        System.out.println(" p = 4, k = 1, s = 1, real = " + result);
+        System.out.println("width = 4, height = 1, s = 1, real = " + result);
     }
 }
